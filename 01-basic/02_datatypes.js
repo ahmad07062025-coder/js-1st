89d1f6b519c9b741c3cf9af0e65765898a968e7e
ahmad07062025-coder <ahmad07062025@gmail.com>
2026-06-06 12:09:05 +0500

    Create 02_datatypes.js

---
+++ 01-basic/02_datatypes.js
@@ -0,0 +1,12 @@
+ "use strict";
+//  alert("Hello World") we are useing node js so we can't use browser alert function
+ let name = "Ahmad Ali"
+ let age = 22
+ let isLoggedIn = false
+ let state; 
+    console.log(typeof name) // string
+    console.log(typeof age) // number
+    console.log(typeof isLoggedIn) // boolean
+    console.log(typeof state) // undefined
+    console.log(typeof null) // object (this is a known bug in JavaScript, null is not actually an object)
+    

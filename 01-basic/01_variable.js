53bc5e255e400ab11d72feca8a13d3b1d9f81e1d
ahmad07062025-coder <ahmad07062025@gmail.com>
2026-06-06 11:53:38 +0500

    Create 01_variable.js

---
+++ 01-basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "ahmadali@google.com"
+var accountPassword ="1234"
+accountCity = "Karachi"
+let accountState;
+let temp = null
+// accountId = 2 not allowed because accountId is a constant variable
+accountEmail = "ahmadali1@google.com"
+accountPassword = "12345"
+accountCity = "Lahore"
+// console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState,temp])
